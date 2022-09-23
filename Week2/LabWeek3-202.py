127c9737e776bbe0a588532ccff8fbe0888eb55d
Lyra Brown <blyra05@gmail.com>
2022-09-23 10:54:43 -0400

    9/23 - Finished 203 section

--- Week2/LabWeek3-202.py
+++ Week2/LabWeek3-202.py
@@ -1,1 +1,70 @@
 
+name = "Lyra Brown"
+message = "Hi there y'all"
+print(name)
+print(message)
+
+a, b, c = 1, 2, -1
+
+# print(b**2-4*a*c)
+print(b ** 2 - 4 * a * c)
+
+a = 0
+
+t = 3
+
+print((-t) ** 2)
+# (-t) (-t)
+# t^2
+
+
+print(-t ** 2)
+# -(t^2)
+
+print(3*3+2)    # 9+2 -> 11         INTEGER
+print(3*(3+2))  # 3*5 -> 15         INTEGER
+print(2+3.0*4)  # 2 + 12.0 -> 14.0  FLOAT
+print(8/4/2)    # 2.0/2 -> 1.0      FLOAT
+print(4/(4/2))  # 4 / 2.0 -> 2.0    FLOAT
+
+print((22.75*(1 + (0.15))))
+print(22.75*(1 + 0.15))
+
+PROF_BIRTH_YEAR = 1969
+
+print(4 % 2)
+print(5 % 2)
+
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+lemons = 3
+limes = 3
+oranges = 6.5
+
+total = lemons + limes + oranges
+print(total)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(t))
+print(type(PROF_BIRTH_YEAR))
+print(type(lemons))
+print(type(limes))
+print(type(oranges))
+print(type(total))
+print(type(name))
+print(type(message))
+
+print("lyra brOwn")
+
+
+
+
+
+
+
+
+
+
