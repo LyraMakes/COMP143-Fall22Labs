90dec5040797d63150b733ddd433928bb3ac27c8
Lyra Brown <blyra05@gmail.com>
2022-10-12 10:48:43 -0400

    10/12 - Ednedned Session

--- Week6/LabWeek6_202.py
+++ Week6/LabWeek6_202.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def main():
-    problem1()
+    problem10()
 
 
 # Problem 1
@@ -9,6 +9,10 @@ def problem1():
     x = 4
     for i in range(3):
         print(x)
+# Output
+# 4
+# 4
+# 4
 
 
 def problem2():
@@ -16,12 +20,22 @@ def problem2():
     for i in range(2):
         print(i)
 
+# Output
+# 0
+# 1
+
 
 # Problem 3
 def problem3():
     x = 6
     for i in range(1, x):
         print('pickles')
+# Output
+# pickles
+# pickles
+# pickles
+# pickles
+# pickles
 
 
 # Problem 4
@@ -29,6 +43,12 @@ def problem4():
     for j in range(1, 3):
         print('Greetings')
         print('\t and salutations')
+# Output
+# Greetings
+#      and salutations
+# Greetings
+#      and salutations
+
 
 # Problem 5
 # What is your understanding of the code `for i in range(10):`?
@@ -42,6 +62,11 @@ def problem6():
     print("x, y\n----")
     for x in range(4):
         print(f"{x}, {2 * x + 1}")
+# Output
+# 0, 1
+# 1, 3
+# 2, 5
+# 3, 7
 
 # Problem 7
 # What is the purpose of the code in (6) above?
@@ -55,6 +80,11 @@ def problem8():
     print("x, y\n----")
     for x in range(4):
         print(f"{x}, {-x ** 2 / 2 + (b + 1 / 2) * x}")
+# Output
+# 0,
+#
+#
+#
 
 
 # Problem 9
@@ -71,6 +101,9 @@ def problem10():
     digits.sort()
     small = digits[0] * 10 ** 3 + digits[1] * 10 ** 2 + digits[2] * 10 + digits[3]
     large = digits[3] * 10 ** 3 + digits[2] * 10 ** 2 + digits[1] * 10 + digits[0]
+    print(f"{large=}")
+    print(f"{small=}")
+
     number = large - small
     print(number)
 
