9c6364f1dab22600a0431ae941f3838a1a960134
Lyra Brown <blyra05@gmail.com>
2022-12-09 10:37:50 -0500

    12/9 - Mid Session

--- Week14/LabWeek14_203.py
+++ Week14/LabWeek14_203.py
@@ -1,6 +1,18 @@
 # Write a function that reads a column of numbers from a file,
 # calculates the average, and writes the average to a file.
 
+# with open("content.txt", "r") as in_file:
+#     content = in_file.readlines()
+#
+# total = 0
+#
+# for line in content:
+#     total += float(line)
+#
+# average = total / len(content)
+#
+# with open("output.txt", "w") as out_file:
+#     out_file.write(f"{average}")
 
 
 # Use recursion to generate a Collatz sequence from a user-specified positive number.
@@ -11,6 +23,16 @@
 # then the sequence will end up at 1.
 
 
+def collatz(num):
+    print(num)
+    if (num % 2 == 0):
+        collatz(num / 2)
+    elif (num > 1):
+        collatz((3 * num) + 1)
+
+
+# print(collatz(17))
+
 
 # (a) Without using a computer, predict the output of this code, given user input values of your choice.
 # (b) What is the purpose of the function?
@@ -18,13 +40,15 @@
 #     Correct the code so that it handles this case correctly.
 # (d) Check all of your work by running code.
 
+def main():
+    total = 0
+    count = 0
+    x = input("Enter a number (q for quit) ")
+    while (x != "q"):
+        total = total + float(x)
+        count += 1
+        x = input("Enter a number (q for quit) ")
+    if (count != 0):
+        print("Result: ", total / count)
 
-# def main():
-#     sum = 0
-#          count = 0
-#          x = input("Enter a number (q for quit) ")
-#     while (x != "q"):
-#         sum = sum + float(x)
-#         count += 1
-#         x = input("Enter a number (q for quit) ")
-#     print ("Result: :", sum/count)
+main()
